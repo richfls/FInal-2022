8806cfc33914b57dd9f7fbca2da2575577faecef
FlzGlz <51001772+richfls@users.noreply.github.com>
2022-12-05 10:03:49 -0700

    Create tiles.py

---
+++ tiles.py
@@ -0,0 +1,14 @@
+import pygame as py
+
+class Tile(py.sprite.Sprite):
+    def __init__(self,pos,size):
+        super().__init__()
+        self.image = py.Surface((size,size))
+        self.image.fill((255,255,255))
+        self.rect = self.image.get_rect(topleft = pos)
+
+
+    #ADDED BY MO- needs finishing
+    def update(self, shift):
+        #print("in the tile update function!")
+        self.rect.x += shift
