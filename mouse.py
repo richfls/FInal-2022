f0ee2fa3ebc4835b003259040b23f934cbee4159
FlzGlz <51001772+richfls@users.noreply.github.com>
2022-12-05 10:03:03 -0700

    Update mouse.py

--- mouse.py
+++ mouse.py
@@ -3,9 +3,12 @@ from settings import *
 
 class Mouse:
     def __init__(self):
-        for event in py.event.get():
-            if event.type == py.QUIT:
-                py.quit()
-            if event.type == py.MOUSEBUTTONDOWN:
-                if py.MOUSEBUTTONDOWN:
-                    print("click")
+        self.mouse_pos = (0, 0)
+        self.click = False
+    def handler(self, event: list[py.event.Event]):
+        if event.type == py.MOUSEMOTION:
+            self.mouse_pos = py.mouse.get_pos()
+        if event.type == py.MOUSEBUTTONDOWN:
+            self.click = True
+        if event.type == py.MOUSEBUTTONUP:
+            self.click = False
