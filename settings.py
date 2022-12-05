c17f4e89e1c37b6be6f5fca1c6046d44f4ba2837
FlzGlz <51001772+richfls@users.noreply.github.com>
2022-12-05 10:02:48 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,16 @@
-SCREEN_WIDTH = 1280
-SCREEN_HIEGHT = 720
+LEVEL_ONE = [
+'                            ',#1
+'                            ',#2
+' xx    xxx             xx   ',#3
+' xx p                       ',#4
+' xxxx        xx          xx ',#5
+' xxxx       xx              ',#6
+' xx    x  xxxx    xx  xx    ',#7
+'       x  xxxx    xx  xxx   ',#8
+'    xxxx  xxxxxx  xx  xxxx  ',#9
+'xxxxxxxx  xxxxxx  xx  xxxx  ']#10
+#1234567890123456789012345679
 
+tile_size = 64
+SCREEN_WIDTH = 1280
+SCREEN_HIEGHT = len(LEVEL_ONE * tile_size)
