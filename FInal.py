3e12d8242d757dcfc3f232382ee156f0f5f083fb
FlzGlz <51001772+richfls@users.noreply.github.com>
2022-12-07 09:52:11 -0700

    Update FInal.py

--- FInal.py
+++ FInal.py
@@ -8,14 +8,12 @@ import sys
 class Game():
     def __init__(self):
         py.init()
-        self.screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HIEGHT))
         self.clock = py.time.Clock()
-        self.level = Level(LEVEL_ONE,self.screen)
-        self.start = False
         #mouse
         self.mouse = Mouse()
         self.click = self.mouse.click
         self.mouse_pos = self.mouse.mouse_pos
+        self.start = start
     def Start(self):
         while self.start == False:
             for event in py.event.get():
@@ -27,17 +25,17 @@ class Game():
                 self.mouse_pos = self.mouse.mouse_pos
                 self.click = self.mouse.click
             #Start
-            py.draw.rect(self.screen,(255,255,255),(SCREEN_WIDTH/2-50,400,100,50))
+            py.draw.rect(screen,(255,255,255),(SCREEN_WIDTH/2-50,400,100,50))
             if ((self.mouse_pos[0] > (SCREEN_WIDTH/2)-50 and self.mouse_pos[0] < (SCREEN_WIDTH/2-50)+100 and
                 (self.mouse_pos[1]) > 400 and self.mouse_pos[1] < 450) and self.click == True ):
                 self.start = True
             #Settings
-            py.draw.rect(self.screen,(255,255,255),(SCREEN_WIDTH/2-50,500,100,50))
+            py.draw.rect(screen,(255,255,255),(SCREEN_WIDTH/2-50,500,100,50))
             if ((self.mouse_pos[0] > (SCREEN_WIDTH/2)-50 and self.mouse_pos[0] < (SCREEN_WIDTH/2-50)+100 and
                 (self.mouse_pos[1]) > 500 and self.mouse_pos[1] < 550) and self.click == True ):
                 pass
             #Quit
-            py.draw.rect(self.screen,(255,255,255),(SCREEN_WIDTH/2-50,600,100,50))
+            py.draw.rect(screen,(255,255,255),(SCREEN_WIDTH/2-50,600,100,50))
             if ((self.mouse_pos[0] > (SCREEN_WIDTH/2)-50 and self.mouse_pos[0] < (SCREEN_WIDTH/2-50)+100 and
                 (self.mouse_pos[1]) > 600 and self.mouse_pos[1] < 650) and self.click == True ):
                 py.quit()
@@ -47,12 +45,13 @@ class Game():
         while self.start == True:
             g.run()
     def run(self):
+        self.level = Level(LEVEL_ONE,screen)
         while True: #GAME-LOOP---------------------------------------
             for event in py.event.get():
                 if event.type == py.QUIT:
                     py.quit()
                     sys.exit()
-            self.screen.fill('black')
+            screen.fill('black')
             self.level.run()
             py.display.update()#WORKS LIKE FLIP BUT CAN UPDAT PORTIONS OF THE SCREEN IF WE WANT
             self.clock.tick(60)
